47dfcdd7bd7fd29aaae400ac33bca2e36d7109c1
320948-AAPS <320948@aaps.k12.mi.us>
2024-01-10 19:46:00 -0500

    Fixed brushless motor

--- mechanism.py
+++ mechanism.py
@@ -10,19 +10,20 @@ import rev
 class Mechanism:
     def __init__(self, config) -> None:
         self.config = config
+        motor_type_brushless = rev.CANSparkMaxLowLevel.MotorType.kBrushless
 
         #motors in the shooter
-        self.leftShootingMotor = rev.CANSparkMax(99,"brushless") #fix the device id later
-        self.rightShootingMotor = rev.CANSparkMax(99,"brushless")
+        self.leftShootingMotor = rev.CANSparkMax(99,motor_type_brushless) #fix the device id later
+        self.rightShootingMotor = rev.CANSparkMax(99,motor_type_brushless)
 
         #intake motor (pulls the notes in)
-        self.intakeMotor = rev.CANSparkMax(99,"brushless")
+        self.intakeMotor = rev.CANSparkMax(99,motor_type_brushless)
 
         #intake up or down motor
-        self.intakeUpDownMotor = rev.CANSparkMax(99,"brushless")
+        self.intakeUpDownMotor = rev.CANSparkMax(99,motor_type_brushless)
 
         #motor that moves the hood
-        self.moveHoodMotor = rev.CANSparkMax(99,"brushless")
+        self.moveHoodMotor = rev.CANSparkMax(99,motor_type_brushless)
         return
     
     def moveHood(self,position):
