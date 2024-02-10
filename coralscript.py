6aab82d273804a4f8407b6b80e0a5f077c929abb
David Loh <lohdavidc@gmail.com>
2024-02-09 21:25:06 -0500

    started incorporating gstreamer

--- coralscript.py
+++ coralscript.py
@@ -1,15 +1,17 @@
+from pycoral.adapters.common import input_size
 from pycoral.adapters.detect import get_objects
 from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter
 from pycoral.utils.edgetpu import run_inference
 from networktables import NetworkTables
+import gstreamer
 
 #see if I need a server (put this in main?)
 NetworkTables.initialize()
 notePub = NetworkTables.getTable('noteDetector')
 
 def getClosestNote(objs):
-    objs.sort(key=lambda bbox: bbox.area)
+    objs.sort(key=lambda obj: obj.bbox.area, reverse=True)
     return objs[0]
 
 def publishBBox(bbox):
@@ -23,14 +25,21 @@ def publishBBox(bbox):
 def main():
     model
     source
+    source_size
+    source_format
     threshold
 
     interpreter = make_interpreter(model)
     interpreter.allocate_tensors()
+    inference_size = input_size(interpreter)
 
-    objs = get_objects(interpreter, threshold)
-    bbox = getClosestNote(objs)
-    publishBBox(bbox)
+    def user_callback(input_tensor, src_size, inference_box):
+        run_inference(interpreter, input_tensor)
+
+        objs = get_objects(interpreter, threshold)
+        publishBBox(getClosestNote(objs))
+
+    gstreamer.run_pipeline(user_callback, src_size=source_size, appsink_size=inference_size, videosrc=source, videofmt=source_format)
     return
 
 if __name__ == '__main__':
