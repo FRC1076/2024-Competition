aee2b464fc71da57b6cd9a5eb7b2f321e8e8c66d
1076software <1076software@gmail.com>
2024-02-12 21:18:03 -0500

    Disable logging, change the default x button angle

--- robot.py
+++ robot.py
@@ -317,7 +317,7 @@ class MyRobot(wpilib.TimedRobot):
         elif self.deadzoneCorrection(self.operator.xboxController.getLeftY(), self.operator.deadzone) < 0:
             self.mechanism.sprocketUp()
         elif self.operator.xboxController.getXButton():
-            self.mechanism.sprocketToPosition(-9) #-25.9 close up #-9 from stage head on
+            self.mechanism.sprocketToPosition(-10) #-25.9 close up #-9 from stage head on
             self.mechanism.indexNote()
         
         else:
