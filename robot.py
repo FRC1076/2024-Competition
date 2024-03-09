2e8ac58cfa81ca8f502cf11ce0eac7f4d500fda0
1076software <1076software@gmail.com>
2024-03-09 14:07:10 -0500

    Updated LED colors (also in Sam vision branch)

--- robot.py
+++ robot.py
@@ -88,8 +88,6 @@ class MyRobot(wpilib.TimedRobot):
         
         self.swervometer.startTimer()
         self.swervometer.initPoseEstimator(self.drivetrain.getModules(), self.vision)
-        self.ledTimer = wpilib.Timer()
-        self.ledTimer.start()
         self.ledOn = True
         return
     
@@ -317,13 +315,7 @@ class MyRobot(wpilib.TimedRobot):
         if not self.mechanism.indexBeamBroken():
             LEDs.rainbowLED("purple")
         else:
-            if(self.ledTimer.get() > 0.1):
-                if(not self.ledOn):
-                    LEDs.rainbowLED("purple")
-                else:
-                    LEDs.rainbowLED("off")
-                self.ledOn = not self.ledOn
-                self.ledTimer.reset()
+            LEDs.rainbowLED("purple-flash")
         return True
     
     def teleopPeriodic(self):
