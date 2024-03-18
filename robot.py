02d634bf3a5fb383cb2f400ea00f1104a24adc34
1076software <1076software@gmail.com>
2024-03-18 11:04:28 -0400

    Skyline day 2

--- robot.py
+++ robot.py
@@ -464,11 +464,11 @@ class MyRobot(wpilib.TimedRobot):
         elif self.operator.xboxController.getBButton():
             distance = -1
             if self.team_is_blu:
-                #distance = self.vision.getAvgDistance()
-                distance = self.swervometer.distanceToPose(-326, 57) - 15 #should be -15 but we are calibrated to be at COF as opposed to front of robot which is used in the auto aim calculations
+                distance = self.vision.getAvgDistance()
+                #distance = self.swervometer.distanceToPose(-326, 57) - 15 #should be -15 but we are calibrated to be at COF as opposed to front of robot which is used in the auto aim calculations
             else:
-                #distance = self.vision.getAvgDistance()
-                distance = self.swervometer.distanceToPose(326, 57) - 15
+                distance = self.vision.getAvgDistance()
+                #distance = self.swervometer.distanceToPose(326, 57) - 15
             if distance != -1 and distance != 0 and distance > 0:
                 self.mechanism.sprocketToPosition(self.mechanism.getAutoAimAngle(distance, 0))
             #print(distance)
